94ee3041b9b0c348901206b9e765ce3062413501
Amal <amal@gmail.com>
2025-03-25 12:47:05 +0530

    add Login page

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "../components/Home";
 import About from "../components/About";
+import Login from "../components/Login";
 
 const router = createBrowserRouter([
   {
@@ -10,6 +11,10 @@ const router = createBrowserRouter([
   {
     path: "/about",
     element: <About />,
+  },
+  {  
+    path: "/login",
+    element: <Login />
   },
 ]);
 
