2304a69c156f84683dc71f6a5e78038074b5fc0d
Amal <amal@gmail.com>
2025-03-29 13:26:36 +0530

    new changes done in register and login page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,10 +3,11 @@ import { useState } from "react";
 import Icon from "../assets/ICON (2).png";
 import email from "../assets/ICON.png";
 import password from "../assets/ICON (1).png";
-import stroke from "../assets/Stroke 3.png";
 import arrow from "../assets/arrow.png";
-import oval from "../assets/oval.png";
+import Oval from "../assets/Oval (2).png"
 import socialmedia from "../assets/Social Media.png"
+import { FaRegEye } from "react-icons/fa6";
+import { FaRegEyeSlash } from "react-icons/fa6";
 
 function Login(){
     const [showPassword, setShowPassword] = useState(false);
@@ -15,28 +16,28 @@ function Login(){
     setShowPassword(!showPassword);
   };
 
-    return <div className="h-[140dvh] w-full bg-gray-300 ">
-                <div className="h-200 w-1/1 flex flex-col justify-center items-center bg-grey-800 text-base/10  ">
+    return <div className="h-screen px-5 w-full bg-gray-300  flex justify-center items-center py-3">
+                <div className="w-full  flex  flex-col justify-center items-center  bg-grey-800 ">
                             <div className="flex gap-2">
                                     <img className="w-15" src={Icon} alt="Login page Logo"/>
-                                    <h5 className="text-2xl mt-1 mr-10 font-semibold">Service Connect</h5>
+                                    <h5 className="text-2xl mt-1 mr-10 font-bold">Service Connect</h5>
                                 </div>
                                         <div className="flex flex-col gap-4 relative">
-                                            <h3 className="text-slate-800 text-2xl mt-10  font-semibold ">Let's Sign In.!</h3>
-                                            <p className="text-sm">Login to Your Account to Continue your Courses</p>
-                                            <img className="w-5 absolute mt-36 ml-4" src={email}/>
-                                            <input className="text-white w-90 bg-stone-500 rounded-lg px-11 py-2" type="text" placeholder="Email" />
-                                            <img className=" w-4 absolute mt-53 ml-5" src={password}/>
-                                            <img className="w-4 absolute mt-54 ml-81 cursor-pointer" src={showPassword ? stroke : stroke} onClick={togglePasswordVisibility}/>
-                                            <input  className="text-white rounded-lg px-11 py-2 bg-stone-500" type={showPassword ? "text" : "password"}  placeholder="Password"/>
+                                            <h3 className="text-slate-800 text-2xl mt-7  font-semibold ">Let's Sign In.!</h3>
+                                            <p className="text-sm ">Login to Your Account to Continue your Courses</p>
+                                            <img className="w-5 absolute mt-33 ml-4" src={email}/>
+                                            <input className="text-white placeholder-white h-14 w-90 bg-stone-500 rounded-lg px-12 py-2" type="text" placeholder="Email" />
+                                            <img className=" w-4 absolute mt-50 ml-4" src={password}/>
+                                            <input  className="text-white  placeholder-white  h-14 rounded-lg px-11 py-2 bg-stone-500" type={showPassword ? "text" : "password"}  placeholder="Password"/>
+                                             <button  onClick={togglePasswordVisibility} className="w-4 absolute mt-51 ml-81 cursor-pointer text-white">{ showPassword ? <FaRegEyeSlash/> : <FaRegEye/> }</button>
                                                 <div className="flex justify-between mt-3">
-                                                        <input className="w-4" type="checkbox"/>
-                                                        <p className="mr-33  text-sm">Remember Me</p>
+                                                        <input className="w-5" type="checkbox"/>
+                                                        <p className="mr-32  text-sm">Remember Me</p>
                                                         <p className="text-sm">Forgot Password?</p>
                                                 </div>
-                                                    <button className="text-white bg-slate-900  p-3 rounded-full cursor-pointer mt-10 realtive">Sign in</button>
-                                                        <img className=" w-13 absolute mx-75 mt-90 " src={oval}/>
-                                                        <img className=" w-6 absolute mx-79 mt-94" src={arrow}/>
+                                                    <button className="text-white bg-slate-900  p-4 rounded-full cursor-pointer mt-5 realtive">Sign In</button>
+                                                        <img className=" w-12 absolute mx-76 mt-82" src={Oval} />
+                                                        <img className=" w-5 absolute mx-80 mt-86" src={arrow}/>
 
                                                         <p className="text-md ml-32">Or Continue With</p>
 
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -3,29 +3,30 @@ import topimg from "../assets/ICON (2).png";
 import email from "../assets/ICON.png";
 import password from "../assets/ICON (1).png";
 import arrow from "../assets/arrow.png";
-import round from "../assets/oval.png";
+import Oval from "../assets/Oval (2).png"
+import socialmedia from "../assets/Social Media.png"
 import { FcGoogle } from "react-icons/fc";
 import { FaRegEye } from "react-icons/fa6";
-import { FaRegEyeSlash } from "react-icons/fa6"
+import { FaRegEyeSlash } from "react-icons/fa6";
 
 function Login() {
   const [showPassword, setShowPassword] = useState(false);
   const [showConfirmPassword, setShowConfirmPassword] = useState(false);
 
   return (
-    <div className="h-screen px-5 w-full bg-gray-300 flex justify-center items-center py-3">
-      <div className="w-full max-w-md bg-gray-300 p-8 mt-12 rounded-xl flex flex-col items-center">
+    <div className="h-screen  w-full bg-gray-300 flex justify-center items-center py-3">
+      <div className="w-full max-w-md bg-gray-300 p-8  rounded-xl flex flex-col items-center">
         <div className="flex items-center gap-3 mb-4">
           <img className="w-16" src={topimg} alt="Login Logo" />
-          <h5 className="text-2xl font-semibold mb-5">Service Connect</h5>
+          <h5 className="text-2xl font-bold mb-5">Service Connect</h5>
         </div>
-        <h3 className="text-2xl mb-6 font-bold flex mr-22 lg:mr-47">Getting Started..!</h3>
+        <h3 className="text-slate-800 text-2xl mb-6 font-semibold flex mr-22 lg:mr-47">Getting Started..!</h3>
         
         <div className="w-full flex flex-col gap-5 relative">
           <div className="relative shadow-sm">
             <img className="w-5 absolute left-4 top-1/2 -translate-y-1/2" src={email} alt="email icon" />
             <input
-              className="w-full bg-stone-600 text-white rounded-lg px-12 py-3 placeholder-white focus:outline-none"
+              className="w-full h-14 bg-stone-500 text-white rounded-lg px-12 py-3 placeholder-white focus:outline-none"
               type="text"
               placeholder="Email"
               onChange={""} name="" id=""
@@ -34,7 +35,7 @@ function Login() {
           <div className="relative">
             <img className="w-4 absolute left-4 top-1/2 -translate-y-1/2" src={password} alt="password icon" />
             <input
-              className="w-full bg-stone-600 text-white rounded-lg px-12 py-3 placeholder-white focus:outline-none"
+              className="w-full h-14 bg-stone-500 text-white rounded-lg px-12 py-3 placeholder-white focus:outline-none"
               type={showPassword ? "text" : "password"}
               placeholder="Password"
               onChange={""} name="" id=""
@@ -50,7 +51,7 @@ function Login() {
           <div className="relative">
             <img className="w-4 absolute left-4 top-1/2 -translate-y-1/2" src={password} alt="password icon" />
             <input
-              className="w-full bg-stone-600 text-white rounded-lg px-12 py-3 placeholder-white focus:outline-none"
+              className="w-full h-14 bg-stone-500 text-white rounded-lg px-12 py-3 placeholder-white focus:outline-none"
               type={showConfirmPassword ? "text" : "password"}
               placeholder="Confirm Password"
               onChange={""} name="" id=""
@@ -69,21 +70,21 @@ function Login() {
               <input type="radio" className="w-4 h-4" /> Agree to Terms & Conditions
             </label>
           </div>
-          <button className="bg-slate-900 text-white py-3 rounded-full mt-2 flex justify-center items-center w-full hover:bg-slate-700 transition relative">
+          <button className="bg-slate-900 text-white py-3 rounded-full h-14  flex justify-center items-center w-full hover:bg-slate-700 transition relative">
             Sign in
             <div className="absolute flex justify-center items-center right-2">
-              <img className="w-10" src={round} alt="oval decoration" />
+              <img className="w-12" src={Oval} alt="oval decoration" />
               <img className="w-5 absolute" src={arrow} alt="arrow icon" />
             </div>
           </button>
           <p className="text-center mt-1">Or Continue With</p>
           <div className="flex justify-center mt-1 cursor-pointer">
-            <div className="bg-gray-400 w-12 h-12 flex justify-center items-center rounded-4xl text-2xl"> 
-              <FcGoogle />
+            <div className="mr-26 cursor-pointer"> 
+               <img className="ml-27 w-40" src={socialmedia}/>
             </div>
           </div>
           <p className="text-center">
-            Don’t have an Account? <a className="underline font-bold hover:text-gray-400" href="#">SIGN UP</a>
+            Don’t have an Account? <a className="underline font-bold hover:text-gray-400" href="#">SIGN IN</a>
           </p>
         </div>
       </div>
