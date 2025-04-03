65e8ad9f70dc382d673173fc522bb8d4a3fa1319
Mohammed fadi <Mohammedfadi1245@gmail.com>
2025-04-03 19:10:44 +0530

    fixed layout issues of pages

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -16,7 +16,7 @@ function Login() {
         <h1 className='pt-5 text-2xl font-semibold ml-2'>Service Connect</h1>
       </div>
       <h1 className='font-bold text-xl my-3 lg:my-1 lg:text-2xl lg:text-center lg:mr-55'>Lets Sign in.!</h1>
-      <p className='text-sm font-semibold text-stone-600 my-3 lg:my-1 lg:text-sm lg:text-center lg:mr-10 lg:my-2'>Login to Your Account to Continue your Courses</p>
+      <p className='text-sm font-semibold text-stone-600 my-3 lg:my-1 lg:text-sm lg:text-center lg:mr-10'>Login to Your Account to Continue your Courses</p>
       <div className='py-1 flex flex-col justify-center items-center gap-2'>
             <ProfileInput  icon={<TfiEmail className="absolute left-4 top-5" size={25} color="white"/>} className='' type="text" placeholder="Email" />
            <ProfileInput  icon={<MdLockOutline className="absolute left-4 top-5" size={25} color="white"/>} className='' type="password" placeholder="Password" />
