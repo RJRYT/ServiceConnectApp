67ab292ad3eb2d2c30ee12409b7db4c53175e142
Alen Salim <alensalim123@gmail.com>
2025-04-07 19:52:50 +0530

    adjusted header size

--- src/components/mainheader.jsx
+++ src/components/mainheader.jsx
@@ -1,32 +1,33 @@
-import React from 'react'
+import React from "react";
 import { IoIosArrowBack } from "react-icons/io";
 import { VscSettings } from "react-icons/vsc";
 import { BiSearchAlt } from "react-icons/bi";
 
-
-function Mainheader({title}) {
+function Mainheader({ title }) {
   return (
     <div>
-      <div className='bg-slate-900 w-full  h-[80px] flex justify-between items-center px-2 lg:px-8'>
-            <div className=' flex items-center lg:gap-4 gap-2'>
-            <div className='border-2 border-white w-13 h-13 rounded-full flex justify-center items-center pr-1'>
-            <IoIosArrowBack className='text-white text-4xl' />
-            </div>
-            <div className=''>
-                <h1 className='text-lg lg:text-2xl font-bold text-white'>{title}</h1>
-            </div>
-            </div>
-            <div className='flex lg:gap-5 gap-2'>
-            <div className='hover:border-1 hover:border-gray-500 duration-300 w-10 h-10 rounded-full flex justify-center items-center pr-1 cursor-pointer lg:w-13 lg:h-13'>
-            <VscSettings className='text-white text-3xl lg:text-4xl' />
-            </div>
-            <div className='hover:border-1 hover:border-gray-500 w-10 h-10 duration-300 rounded-full flex justify-center items-center  cursor-pointer lg:w-13 lg:h-13'>
-            <BiSearchAlt className='text-white text-3xl lg:text-4xl' />
-            </div>   
-            </div>
+      <div className="bg-slate-900 w-full  h-[80px] flex justify-between items-center px-2 lg:px-8">
+        <div className=" flex items-center lg:gap-4 gap-2">
+          <div className="border-2 border-white w-10 h-10 lg:w-13 lg:h-13 rounded-full flex justify-center items-center pr-1">
+            <IoIosArrowBack className="text-white text-3xl" />
+          </div>
+          <div className="">
+            <h1 className="text-lg lg:text-2xl font-bold text-white">
+              {title}
+            </h1>
+          </div>
+        </div>
+        <div className="flex lg:gap-5 gap-2">
+          <div className="hover:border-1 hover:border-gray-500 duration-300 w-10 h-10 rounded-full flex justify-center items-center pr-1 cursor-pointer lg:w-13 lg:h-13">
+            <VscSettings className="text-white text-3xl lg:text-4xl" />
+          </div>
+          <div className="hover:border-1 hover:border-gray-500 w-10 h-10 duration-300 rounded-full flex justify-center items-center  cursor-pointer lg:w-13 lg:h-13">
+            <BiSearchAlt className="text-white text-3xl lg:text-4xl" />
+          </div>
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Mainheader
+export default Mainheader;
--- src/pages/allcategory.jsx
+++ src/pages/allcategory.jsx
@@ -11,7 +11,12 @@ function Allcategory() {
       <section className="h-screen bg-gray-300">
         <div className="grid grid-cols-3 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-7 gap-3 sm:gap-y-10 place-items-center max-h-screen bg-gray-300 p-4">
           {allCategories.map((item) => (
-            <MenuItem key={item.title} image={item.image} title={item.title} />
+            <MenuItem
+              key={item.title}
+              image={item.image}
+              title={item.title}
+              link={item.link}
+            />
           ))}
         </div>
       </section>
