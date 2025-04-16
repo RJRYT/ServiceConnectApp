31780c21320a44af8144c55db8aabe60f98ac615
Mohammed fadi <Mohammedfadi1245@gmail.com>
2025-04-16 19:43:55 +0530

    created Eript page

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -16,6 +16,7 @@ import Test from "../components/test";
 import DashBoard from "../layouts/DashBoard";
 import Bookings from "../pages/bookings";
 import Allcategory from "../pages/allcategory";
+import Erecipt from "../pages/Erecipt";
 
 const router = createBrowserRouter([
   {
@@ -88,6 +89,10 @@ const router = createBrowserRouter([
     path: "bookings",
     element: <Bookings />,
   },
+  {
+    path: "Recipt",
+    element: <Erecipt/>
+  }
 ]);
 
 export default function Router() {
