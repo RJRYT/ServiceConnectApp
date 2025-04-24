4e3d9730fa0e6887231706db2ca3c00021fb7d15
Mohammed fadi <Mohammedfadi1245@gmail.com>
2025-04-24 19:49:10 +0530

    fixed positioning

--- src/components/togglebutton.jsx
+++ src/components/togglebutton.jsx
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const ToggleSwitch = ({ isOn, handleToggle }) => {
+const ToggleSwitch = () => {
+  const [isOn, setIsOn] = useState(false);
+
+  const handleToggle = () => {
+    setIsOn(prev => !prev);
+  };
+
   return (
-    <label className="relative inline-block w-12 h-6 cursor-pointer">
-      <input
-        type="checkbox"
-        checked={isOn}
-        onChange={handleToggle}
-        className="sr-only peer"
-      />
-      <div className="bg-blue-100 peer-checked:bg-blue-200 rounded-full w-full h-full transition-colors duration-300 shadow"></div>
-      <div className="absolute top-1/2 left-[3px] w-5 h-5 bg-black rounded-full transition-transform duration-300 transform -translate-y-1/2 peer-checked:translate-x-6"></div>
-    </label>
+    <div onClick={handleToggle} className="relative inline-block w-12 h-6 cursor-pointer">
+      <div
+        className={`rounded-full w-full h-full transition-colors duration-300 shadow ${
+          isOn ? 'bg-blue-200' : 'bg-blue-100'
+        }`}
+      ></div>
+
+      <div
+        className={`absolute top-1/2 w-5 h-5 bg-black rounded-full transition-transform duration-300 transform -translate-y-1/2 ${
+          isOn ? 'translate-x-6' : 'translate-x-[3px]'
+        }`}
+      ></div>
+    </div>
   );
 };
 
